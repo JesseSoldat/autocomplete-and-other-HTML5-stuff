0b6ed398f7bd7af8a96f3f07f2008f5eb899feee
Jesse Soldat <jsoldat@hotmail.com>
2015-12-26 11:17:16 -0500

    created a triangle and a rectangle

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // window.onload = draw1;
-
 //------------------------------------
 //CANVAS 1
 function draw1 () {
@@ -35,11 +34,37 @@ function draw3(w, h) {
   var ctx3 = canvas3.getContext('2d');
   ctx3.clearRect(0,0,550,400);
   ctx3.fillStyle = "rgba(255,0,0,1)";
-  ctx3.fillRect(100,100,w,h);
+  ctx3.fillRect(100,50,w,h);
   w += 1;
-  h += 1;
+  h += 1.5;
   var loopTimer2 = setTimeout('draw3('+w+','+h+')');
-  if (w === 200) {
+  if (w === 250 || h === 300) {
     clearTimeout(loopTimer2);
   };
+}
+function draw4() {
+  var canvas4 = document.getElementById('canvas4');
+  var ctx4 = canvas4.getContext('2d');
+  ctx4.clearRect(0,0,400,300);
+  ctx4.beginPath();
+  ctx4.moveTo(100,50);
+  ctx4.lineTo(130,100);
+  ctx4.lineTo(70,100);
+  ctx4.fillStyle = 'rgba(200,200,0,1)';
+  ctx4.fill();
+
+  ctx4.beginPath();
+  ctx4.moveTo(100,200);
+  ctx4.lineTo(200,200);
+  ctx4.lineTo(200,250);
+  ctx4.lineTo(100,250);
+  ctx4.fillStyle = 'rgba(0,200,200,1)';
+  ctx4.fill();
+
+
+
+
+  
+
+
 }
